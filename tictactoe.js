71694458f2d3ca71fc3d615cda5a52b792e27767
AnanSarah <anansarahnikita27@gmail.com>
2018-03-19 16:38:44 +0600

    Resubmission

--- tictactoe.js
+++ tictactoe.js
@@ -11,6 +11,9 @@ function play(event){
   console.log(event);
     if(!event.target.innerText){
     event.target.innerText= turn? "O":"X";
+    checkWinner();
+    turn =!turn;
+
     //countFunc++ ;
 //any winning state occurs after 5 turns
     /*if(countFunc>4){
@@ -20,6 +23,7 @@ function play(event){
     } bleh this aint working
       */
 }
+}
 
 //8 winning conditions
 function checkWinner() {
@@ -27,32 +31,48 @@ function checkWinner() {
 //1st row
   if( (document.getElementById("0").innerHTML == document.getElementById("1").innerHTML) && (document.getElementById("0").innerHTML == document.getElementById("2").innerHTML ) && document.getElementById("0").innerHTML != "" ){
     console.log("1st row");
+    if(turn == false){
+      console.log("player1 Won");
+      p1Won++;
+      document.getElementById("GameStatus").innerHTML = "Player 1 wins!";
+      document.getElementById("p1Win").innerHTML = p1Won;
+      p2Lost++;
+      document.getElementById("p2Loss").innerHTML = p2Lost;
 
-    if(turn == false){console.log("player1 Won"); p1Won++;  document.getElementById("p1Win").innerHTML = p1Won; p2Lost++; document.getElementById("p2Loss").innerHTML = p2Lost;}
+    }
     else {
         console.log(
           "player2"
         );
         p2Won++;
-        document.getElementById("p2Win") = p2Won;
+        document.getElementById("GameStatus").innerHTML = "Player 2 wins!";
+        document.getElementById("p2Win").innerHTML = p2Won;
         p1Lost++;
-        document.getElementById.innerHTML("p1Loss") = p1Lost;
-
+        document.getElementById("p1Loss").innerHTML = p1Lost;
     }
   }
 
 //2nd row
   else if (document.getElementById("3").innerHTML == document.getElementById("4").innerHTML && document.getElementById("4").innerHTML == document.getElementById("5").innerHTML && document.getElementById("5").innerHTML != "") {
     console.log("2nd row");
-    if(turn == false){console.log("player1"); p1Won++;  document.getElementById("p1Win").innerHTML = p1Won;  p2Lost++;  document.getElementById("p2Loss").innerHTML = p2Lost;}
+    if(turn == false){
+      console.log("player1");
+      p1Won++;
+      document.getElementById("GameStatus").innerHTML = "Player 1 wins!";
+      document.getElementById("p1Win").innerHTML = p1Won;
+      p2Lost++;
+      document.getElementById("p2Loss").innerHTML = p2Lost;
+
+    }
     else {
         console.log(
           "player2"
         );
         p2Won++;
-        document.getElementById("p2Win") = p2Won;
+        document.getElementById("GameStatus").innerHTML = "Player 2 wins!";
+        document.getElementById("p2Win").innerHTML = p2Won;
         p1Lost++;
-        document.getElementById.innerHTML("p1Loss") = p1Lost;
+        document.getElementById("p1Loss").innerHTML = p1Lost;
 
     }
   }
@@ -60,15 +80,23 @@ function checkWinner() {
 //3rd row
   else if (document.getElementById("6").innerHTML == document.getElementById("7").innerHTML && document.getElementById("7").innerHTML == document.getElementById("8").innerHTML && document.getElementById("8").innerHTML != "") {
     console.log("3rd row");
-    if(turn == false){console.log("player1"); p1Won++;  document.getElementById("p1Win").innerHTML = p1Won;  p2Lost++;  document.getElementById("p2Loss").innerHTML = p2Lost;}
+    if(turn == false){
+      console.log("player1");
+      p1Won++;
+      document.getElementById("GameStatus").innerHTML = "Player 1 wins!";
+      document.getElementById("p1Win").innerHTML = p1Won;
+      p2Lost++;
+      document.getElementById("p2Loss").innerHTML = p2Lost;
+    }
     else {
         console.log(
           "player2"
         );
         p2Won++;
-        document.getElementById("p2Win") = p2Won;
+        document.getElementById("GameStatus").innerHTML = "Player 2 wins!";
+        document.getElementById("p2Win").innerHTML = p2Won;
         p1Lost++;
-        document.getElementById.innerHTML("p1Loss") = p1Lost;
+        document.getElementById("p1Loss").innerHTML = p1Lost;
 
     }
   }
@@ -76,15 +104,23 @@ function checkWinner() {
 //1st col
   else if (document.getElementById("0").innerHTML == document.getElementById("3").innerHTML && document.getElementById("3").innerHTML == document.getElementById("6").innerHTML && document.getElementById("6").innerHTML != "") {
     console.log("1st col");
-    if(turn == false){console.log("player1"); p1Won++;  document.getElementById("p1Win").innerHTML = p1Won;  p2Lost++;  document.getElementById("p2Loss").innerHTML = p2Lost;}
+    if(turn == false){
+      console.log("player1");
+      p1Won++;
+      document.getElementById("GameStatus").innerHTML = "Player 1 wins!";
+      document.getElementById("p1Win").innerHTML = p1Won;
+      p2Lost++;
+      document.getElementById("p2Loss").innerHTML = p2Lost;
+    }
     else {
         console.log(
           "player2"
         );
-        p2Won++;
-        document.getElementById("p2Win") = p2Won;
-        p1Lost++;
-        document.getElementById.innerHTML("p1Loss") = p1Lost;
+            p2Won++;
+            document.getElementById("GameStatus").innerHTML = "Player 2 wins!";
+            document.getElementById("p2Win").innerHTML = p2Won;
+            p1Lost++;
+            document.getElementById("p1Loss").innerHTML = p1Lost;
 
     }
   }
@@ -92,16 +128,24 @@ function checkWinner() {
 //2nd col
     else if (document.getElementById("1").innerHTML == document.getElementById("4").innerHTML && document.getElementById("4").innerHTML == document.getElementById("7").innerHTML && document.getElementById("7").innerHTML != "") {
       console.log("2nd col");
-      if(turn == false){console.log("player1"); p1Won++;  document.getElementById("p1Win").innerHTML = p1Won;  p2Lost++;  document.getElementById("p2Loss").innerHTML = p2Lost;}
+      if(turn == false){
+        console.log("player1");
+        p1Won++;
+        document.getElementById("GameStatus").innerHTML = "Player 1 wins!";
+        document.getElementById("p1Win").innerHTML = p1Won;
+        p2Lost++;
+        document.getElementById("p2Loss").innerHTML = p2Lost;
+      }
       else {
           console.log(
             "player2"
           );
 
           p2Won++;
-          document.getElementById("p2Win") = p2Won;
+          document.getElementById("GameStatus").innerHTML = "Player 2 wins!";
+          document.getElementById("p2Win").innerHTML = p2Won;
           p1Lost++;
-          document.getElementById.innerHTML("p1Loss") = p1Lost;
+          document.getElementById("p1Loss").innerHTML = p1Lost;
 
       }
   }
@@ -109,55 +153,80 @@ function checkWinner() {
 //3rd col
   else if (document.getElementById("2").innerHTML == document.getElementById("5").innerHTML && document.getElementById("5").innerHTML == document.getElementById("8").innerHTML && document.getElementById("8").innerHTML != "") {
     console.log("3rd col");
-    if(turn == false){console.log("player1"); p1Won++;  document.getElementById("p1Win").innerHTML = p1Won;  p2Lost++;  document.getElementById("p2Loss").innerHTML = p2Lost;}
+    if(turn == false){
+      console.log("player1");
+      p1Won++;
+      document.getElementById("GameStatus").innerHTML = "Player 1 wins!";
+      document.getElementById("p1Win").innerHTML = p1Won;
+      p2Lost++;
+      document.getElementById("p2Loss").innerHTML = p2Lost;}
     else {
         console.log(
           "player2"
         );
         p2Won++;
-        document.getElementById("p2Win") = p2Won;
+        document.getElementById("GameStatus").innerHTML = "Player 2 wins!";
+        document.getElementById("p2Win").innerHTML = p2Won;
         p1Lost++;
-        document.getElementById.innerHTML("p1Loss") = p1Lost;
+        document.getElementById("p1Loss").innerHTML = p1Lost;
       }
   }
 
 //1st diagonal
     else if (document.getElementById("0").innerHTML == document.getElementById("4").innerHTML && document.getElementById("4").innerHTML == document.getElementById("8").innerHTML && document.getElementById("8").innerHTML != "") {
       console.log("1st diagon alley");
-      if(turn == false){console.log("player1"); p1Won++;  document.getElementById("p1Win").innerHTML = p1Won;  p2Lost++;  document.getElementById("p2Loss").innerHTML = p2Lost;}
+      if(turn == false){
+        console.log("player1");
+        p1Won++;
+        document.getElementById("GameStatus").innerHTML = "Player 1 wins!";
+        document.getElementById("p1Win").innerHTML = p1Won;
+        p2Lost++;
+        document.getElementById("p2Loss").innerHTML = p2Lost;
+      }
       else {
           console.log(
             "player2"
           );
           p2Won++;
-          document.getElementById("p2Win") = p2Won;
+          document.getElementById("GameStatus").innerHTML = "Player 2 wins!";
+          document.getElementById("p2Win").innerHTML = p2Won;
           p1Lost++;
-          document.getElementById.innerHTML("p1Loss") = p1Lost;
+          document.getElementById("p1Loss").innerHTML = p1Lost;
         }
   }
 
 //2nd diagonal
     else if (document.getElementById("2").innerHTML == document.getElementById("4").innerHTML && document.getElementById("4").innerHTML == document.getElementById("6").innerHTML && document.getElementById("6").innerHTML != "") {
       console.log("2nd diagon alley");
-      if(turn == false){console.log("player1"); p1Won++; document.getElementById("p1Win").innerHTML = p1Won;  p2Lost++;  document.getElementById("p2Loss").innerHTML = p2Lost;}
+      if(turn == false){
+        console.log("player1");
+        p1Won++;
+        document.getElementById("GameStatus").innerHTML = "Player 1 wins!";
+        document.getElementById("p1Win").innerHTML = p1Won;
+        p2Lost++;
+        document.getElementById("p2Loss").innerHTML = p2Lost;
+      }
       else {
           console.log(
             "player2"
           );
           p2Won++;
-          document.getElementById("p2Win") = p2Won;
+          document.getElementById("GameStatus").innerHTML = "Player 2 wins!";
+          document.getElementById("p2Win").innerHTML = p2Won;
           p1Lost++;
-          document.getElementById.innerHTML("p1Loss") = p1Lost;
+          document.getElementById("p1Loss").innerHTML = p1Lost;
 
       }
   }
 }
-function restart(){
-  var x = document.getElementById("tictactoe_table").cells
-  for (var i = 0; i < 10; i++) {
-      x[i].innerHTML ="";
+function restartGame(){
+  var x = document.getElementById("tictactoe_table").cells;
+  for (var i = 0; i < 9; i++) {
+      document.getElementById(i).innerText ="";
+}
 }
 
-document.getElementById("restart").addEventListener("click",function(event){
-  restart();
+document.getElementById("restart").addEventListener("click",function (event){
+  console.log("workin?");
+  restartGame();
 })
